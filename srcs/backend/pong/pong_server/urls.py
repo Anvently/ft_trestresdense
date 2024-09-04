dfac9a04487889034677bbb6ddc601a3236dd023
Nicolas Pirard <pirard.nicolas@hotmail.fr>
2024-09-04 16:39:28 +0200

    added pong api

--- srcs/backend/pong/pong_server/urls.py
+++ srcs/backend/pong/pong_server/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import PostGameView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('init-game/', PostGameView.as_view())
 ]
