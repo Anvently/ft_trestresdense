43df8acf940fbdfdae444b5238f4c76a654e1a50
Nicolas Pirard <pirard.nicolas@hotmail.fr>
2024-09-05 15:43:13 +0200

    consumer and comments edited

--- srcs/backend/pong/pong_server/authentication.py
+++ srcs/backend/pong/pong_server/authentication.py
@@ -1,7 +1,7 @@
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.permissions import BasePermission
-from typing import List
+from typing import List, Dict
 from channels.middleware import BaseMiddleware
 import time
 import jwt
@@ -54,29 +54,6 @@ class ApiJWTAuthentication(BaseAuthentication):
 	
 	def authenticate_header(self, request):
 		return "Bearer"
-
-class JWTAuthMiddleware(BaseMiddleware):
-	
-	async def __call__(self, scope, receive, send):
-	
-		scope['username'] = None
-		# headers = scope.get('headers')
-		# if not headers:
-		# 	scope['error'] = 'cookie auth-token not provided'
-		# 	return
-		# token = headers.get('auth-token', None)
-		# if token:
-		# 	try:
-		# 		data = verify_jwt(token, True)
-		# 		scope['username'] = data['username']
-		# 	except:
-		# 		scope['error'] = 'token verification failed'
-
-		# else:
-		# 	scope['error'] = 'provide an auth token'
-		scope['username'] = "pouet"
-	
-		return await super().__call__(scope, receive, send)
 
 
 # class CookieUserJWTAuthentication(BaseAuthentication):
