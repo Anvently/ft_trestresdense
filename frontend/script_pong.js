489036266495e41a6e12b9648b762fb340bc43b2
Memli Sheremeti <mshereme@student.42.fr>
2024-09-06 16:45:43 +0200

    ADD of router for spa and game script.js

--- frontend/script_pong.js
+++ frontend/script_pong.js
@@ -1,38 +1,49 @@
-const containerCanva = /**  @type {HTMLCanvasElement} */  document.getElementById("container-canva");
-var canvas = document.getElementById("canvas");
+const 	containerCanva = /**  @type {HTMLCanvasElement} */  document.getElementById("container-canva");
+var 	canvas = document.getElementById("canvas");
+const 	usernameButt = document.getElementById("username-field");
+const 	lobbyID = document.getElementById("lobby-id");
+const 	loginBUTT = document.getElementById("submit-btn");
+// const	wsRef = null;
 
-canvas.width = containerCanva.clientWidth;
-canvas.height = containerCanva.clientHeight;
+// loginBUTT.addEventListener("click", e => {
+//     e.preventDefault();
+// });
 
-// Constants
-const WEST = 0;
-const EAST = 1;
-const NORTH = 2;
-const SOUTH = 3;
+const wsRef = new WebSocket(
+	'wss://'
+	+ 'localhost:8083'
+	+ `/ws/pong/10/`
+);
 
 
-var players = [
+
+	canvas.width = containerCanva.clientWidth;
+	canvas.height = containerCanva.clientHeight;
+	
+	// Constants
+	const WEST = 0;
+	const EAST = 1;
+	const NORTH = 2;
+	const SOUTH = 3;
+	
+	
+	var players = [
 	{type: "wall", lives: 0, x: 0, y: 0, width: 0, height: 0}
 ];
 var ball = {x: 0.5, y: 0.5, r: 0, speedX: 0, speedY: 0};
 var number_of_players;
 
-const wsRef = new WebSocket(
-	'wss://'
-	+ 'localhost:8083'
-	+ '/ws/pong/square/'
-);
 
 function draw()
 {
 	var context = canvas.getContext("2d");
-
+	
 	// draw field
 	context.fillStyle = 'grey';
 	context.fillRect(0, 0, canvas.width, canvas.height);
 
 	// draw players
-	for (i = 0; i < number_of_players; i++)
+	for (var i = 0; i < number_of_players; i++)
 	{
 		// paddle color
 		switch (i)
@@ -43,12 +54,12 @@ function draw()
 			case (EAST):
 				context.fillStyle = 'red';
 				break;
-			case (NORTH):
+				case (NORTH):
 				context.fillStyle = 'green';
 				break;
-			case (SOUTH):
-				context.fillStyle = 'yellow';
-				break;
+				case (SOUTH):
+					context.fillStyle = 'yellow';
+					break;
 		}
 		// draw paddle
 		context.fillRect((players[i].x - players[i].width / 2) * canvas.width,
@@ -61,7 +72,7 @@ function draw()
 	context.beginPath(); // ???
 	context.fillStyle = 'black';
 	context.arc(ball.x * canvas.width,
-				ball.y * canvas.height,
+		ball.y * canvas.height,
 				ball.r * canvas.height,
 				0,
 				Math.PI * 2,
@@ -85,7 +96,7 @@ wsRef.onmessage = function (e) {
 	if (msg.hasOwnProperty("ball_speed_y"))
 		ball.speedY = parseFloat(msg.ball_speed_y)
 
-	for (i = 0; i < number_of_players; i++)
+	for (var i = 0; i < number_of_players; i++)
 	{
 		players[i] = {
 					type: msg[`player${i}_type`],
@@ -95,14 +106,14 @@ wsRef.onmessage = function (e) {
 					width: msg[`player${i}_width`],
 					height: msg[`player${i}_height`]
 				};
-	}
-}
+			}
+		}
 
-// INPUT
-var pressKey = {
-	key_up: false,
-	key_down: false,
-};
+		// INPUT
+		var pressKey = {
+			key_up: false,
+			key_down: false,
+		};
 
 window.addEventListener("keydown", e => {
 	console.log(e.key);
@@ -110,7 +121,7 @@ window.addEventListener("keydown", e => {
 		pressKey.key_up = true;
 	else if (e.key === "ArrowDown") 
 		pressKey.key_down = true;
-
+	
 });
 
 window.addEventListener("keyup", e => {
@@ -123,9 +134,9 @@ window.addEventListener("keyup", e => {
 
 setInterval(() => {
 	if (pressKey.up_key === true)
-		wsRef.send(JSON.stringify({ key_pressed: "up" }));
+		wsRef.send(JSON.stringify({ login:"" key_pressed: "up" }));
 	if (pressKey.down_key === true)
 		wsRef.send(JSON.stringify({ key_pressed: "down" }));
-
+	
 	draw();
 }, 20);
