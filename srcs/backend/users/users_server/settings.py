234c2cdc3441442797090f833da9b089cddee0ec
Nicolas Pirard <pirard.nicolas@hotmail.fr>
2024-09-20 10:47:09 +0200

    added healthchecks

--- srcs/backend/users/users_server/settings.py
+++ srcs/backend/users/users_server/settings.py
@@ -81,8 +81,6 @@ WSGI_APPLICATION = 'users_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-ALLOWED_APIS = os.getenv('ALLOWED_APIS').split(',')
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
