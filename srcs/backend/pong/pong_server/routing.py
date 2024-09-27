940ad4053f945997e80fd2b7111bf4cf5ca763a1
Nicolas Pirard <pirard.nicolas@hotmail.fr>
2024-09-27 11:39:51 +0200

    changed regex

--- srcs/backend/pong/pong_server/routing.py
+++ srcs/backend/pong/pong_server/routing.py
@@ -3,7 +3,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-	re_path(r'ws/pong/(?P<lobby_id>\w+(?:\.\d+)*)/$', consumers.PongConsumer.as_asgi()),
+	re_path(r'ws/pong/(?P<lobby_id>[^/]+)/$', consumers.PongConsumer.as_asgi()),
     # re_path(r'ws/pong/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
 
